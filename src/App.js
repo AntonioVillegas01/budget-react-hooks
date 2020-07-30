bc0f9dcbdc8e1cbfeae7cf8bb137a233dbbc3e2a
Marco Garcia <antonio_villegas@hotmail.com>
2020-07-30 16:48:14 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
+    //Definir el state
+    const[presupuesto,guardarPresupuesto] = useState(0);
+    const[restante,guardarRestante] = useState(0);
+    const[mostrarPregunta,actualizarPregunta] = useState(true);
+    const[gastos,guardarGastos] = useState([]);
+    const[gasto,guardarGasto] = useState({});
+    const[creargasto,guardarCrearGasto] = useState(false);
+
+    //useEffect que acxtualiza el restante
+    useEffect( () => {
+        if(creargasto){
+
+            //agrega  nuevo presupuesto
+            guardarGastos([
+                ...gastos,
+                gasto,
+            ]);
+
+            //Resta el presupuesto
+            const presupuestoRestante = restante - gasto.cantidad;
+            guardarRestante(presupuestoRestante);
+
+            //RESETEAR a false
+            guardarCrearGasto(false)
+        }
+    }, [gasto,creargasto,gastos, restante]);
+
+
+    useEffect(() => {
+        if(creargasto) {
+
+            // agrega el nuevo presupuesto
+            guardarGastos([
+                ...gastos,
+                gasto
+            ]);
+
+            // resta del presupuesto actual
+            const presupuestoRestante = restante - gasto.cantidad;
+            guardarRestante(presupuestoRestante);
+
+            // Resetear a false
+            guardarCrearGasto(false);
+        }
+    }, [gasto, creargasto, gastos, restante]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container">
+        <header>
+          <h1>Presupuesto</h1>
+        </header>
+          <div className="contenido-principal contenido">
+              {/* Agregamos componente de carga condicional con parentesis para el return implicito*/}
+              {mostrarPregunta
+                  ? (<Pregunta
+                  guardarPresupuesto={guardarPresupuesto}
+                  guardarRestante={guardarRestante}
+                  actualizarPregunta={actualizarPregunta}
+              />)
+                  : (
+                      <div className="row">
+                          <div className="one-half column">
+                              <Formulario
+                                  guardarGasto={guardarGasto}
+                                  guardarCrearGasto={guardarCrearGasto}
+                              />
+                          </div>
+                          <div className="one-half column">
+                              <Listado
+                                  gastos={gastos}
+                              />
+                              <ControlPresupuesto
+                                  presupuesto={presupuesto}
+                                  restante={restante}
+                              />
+                          </div>
+                      </div>
+                  )}
+          </div>
+      </div>
   );
 }
 
